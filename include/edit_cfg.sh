6a17cc6c74011c9a446644bc762f7b3525be280d
Thomas E. Dickey <dickey@invisible-island.net>
1996-10-06 02:07:44 -0400

    snapshot of project "ncurses", label v1_9_9e_e321_961005

---
+++ include/edit_cfg.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Edit the default value of the term.h file based on the autoconf-generated
+# values:
+#
+#	$1 = config.h
+#	$2 = term.h
+#
+for name in \
+	HAVE_TCGETATTR \
+	HAVE_TERMIOS_H \
+	HAVE_TERMIO_H \
+	BROKEN_LINKER
+do
+	mv $2 $2.bak
+	if ( grep "[ 	]$name[ 	]" $1 2>&1 >/dev/null )
+	then
+		sed -e 's/define '$name'.*$/define '$name' 1/' $2.bak >$2
+	else
+		sed -e 's/define '$name'.*$/define '$name' 0/' $2.bak >$2
+	fi
+	if (cmp -s $2 $2.bak)
+	then
+		echo '** same: '$name
+		mv $2.bak $2
+	else
+		echo '** edit: '$name
+		rm -f $2.bak
+	fi
+done
