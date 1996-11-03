624738afac48411d00d079b52edda8e20efb4872
Thomas E. Dickey <dickey@invisible-island.net>
1996-11-03 02:30:42 -0500

    snapshot of project "ncurses", label v1_9_9e_e322_961102

---
+++ ncurses/lib_delwin.c
@@ -0,0 +1,44 @@
+
+/***************************************************************************
+*                            COPYRIGHT NOTICE                              *
+****************************************************************************
+*                ncurses is copyright (C) 1992-1995                        *
+*                          Zeyd M. Ben-Halim                               *
+*                          zmbenhal@netcom.com                             *
+*                          Eric S. Raymond                                 *
+*                          esr@snark.thyrsus.com                           *
+*                                                                          *
+*        Permission is hereby granted to reproduce and distribute ncurses  *
+*        by any means and for any fee, whether alone or as part of a       *
+*        larger distribution, in source or in binary form, PROVIDED        *
+*        this notice is included with any such distribution, and is not    *
+*        removed from any of its header files. Mention of ncurses in any   *
+*        applications linked with it is highly appreciated.                *
+*                                                                          *
+*        ncurses comes AS IS with no warranty, implied or expressed.       *
+*                                                                          *
+***************************************************************************/
+
+/*
+**	lib_delwin.c
+**
+**	The routine delwin().
+**
+*/
+
+#include <curses.priv.h>
+
+MODULE_ID("$Id: lib_delwin.c,v 1.6 1996/10/05 20:37:33 tom Exp $")
+
+int delwin(WINDOW *win)
+{
+	T(("delwin(%p) called", win));
+
+	if (win == 0)
+	    return(ERR);
+
+	touchwin((win->_flags & _SUBWIN) ? win->_parent : curscr);
+	_nc_freewin(win);
+
+	return(OK);
+}
---
+++ ncurses/lib_longname.c
@@ -0,0 +1,47 @@
+
+/***************************************************************************
+*                            COPYRIGHT NOTICE                              *
+****************************************************************************
+*                ncurses is copyright (C) 1992-1995                        *
+*                          Zeyd M. Ben-Halim                               *
+*                          zmbenhal@netcom.com                             *
+*                          Eric S. Raymond                                 *
+*                          esr@snark.thyrsus.com                           *
+*                                                                          *
+*        Permission is hereby granted to reproduce and distribute ncurses  *
+*        by any means and for any fee, whether alone or as part of a       *
+*        larger distribution, in source or in binary form, PROVIDED        *
+*        this notice is included with any such distribution, and is not    *
+*        removed from any of its header files. Mention of ncurses in any   *
+*        applications linked with it is highly appreciated.                *
+*                                                                          *
+*        ncurses comes AS IS with no warranty, implied or expressed.       *
+*                                                                          *
+***************************************************************************/
+
+
+/*
+**	lib_longname.c
+**
+**	The routine longname().
+**
+*/
+
+#include <curses.priv.h>
+#include <string.h>
+
+MODULE_ID("$Id: lib_longname.c,v 1.4 1996/07/31 00:25:23 tom Exp $")
+
+char *
+longname(void)
+{
+char	*ptr;
+
+	T(("longname() called"));
+
+	for (ptr = ttytype + strlen(ttytype); ptr > ttytype; ptr--)
+		if (*ptr == '|')
+			return(ptr + 1);
+
+    return(ttytype);
+}
