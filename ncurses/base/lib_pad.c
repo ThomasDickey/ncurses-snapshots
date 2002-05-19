78486f8b4bbaf1bda2e34e63117c915e6edd9c87
Thomas E. Dickey <dickey@invisible-island.net>
2002-05-18 21:48:53 -0400

    snapshot of project "ncurses", label v5_2_20020518

--- ncurses/base/lib_pad.c
+++ ncurses/base/lib_pad.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (c) 1998,2000,2001 Free Software Foundation, Inc.                   *
+ * Copyright (c) 1998-2001,2002 Free Software Foundation, Inc.                   *
  *                                                                          *
  * Permission is hereby granted, free of charge, to any person obtaining a  *
  * copy of this software and associated documentation files (the            *
@@ -40,7 +40,7 @@
 
 #include <curses.priv.h>
 
-MODULE_ID("$Id: lib_pad.c,v 1.35 2001/06/03 00:39:24 skimo Exp $")
+MODULE_ID("$Id: lib_pad.c,v 1.36 2002/05/18 21:28:44 Kriang.Lerdsuwanakij Exp $")
 
 NCURSES_EXPORT(WINDOW *)
 newpad(int l, int c)
@@ -193,8 +193,22 @@ pnoutrefresh
 	 i++, m++) {
 	register struct ldat *nline = &newscr->_line[m];
 	register struct ldat *oline = &win->_line[i];
+	NCURSES_CH_T ch;
 
-	for (j = pmincol, n = smincol; j <= pmaxcol; j++, n++) {
+	/*
+	 * Special case for leftmost character of the displayed area.
+	 * Only half of a double-width character may be visible.
+	 */
+	ch = oline->text[pmincol];
+	if_WIDEC(isnac(ch)) {
+	    SetChar(ch, L(' '), AttrOf(oline->text[pmincol - 1]));
+	}
+	if (!CharEq(ch, nline->text[smincol])) {
+	    nline->text[smincol] = ch;
+	    CHANGED_CELL(nline, smincol);
+	}
+
+	for (j = pmincol + 1, n = smincol + 1; j <= pmaxcol; j++, n++) {
 	    if (!CharEq(oline->text[j], nline->text[n])) {
 		nline->text[n] = oline->text[j];
 		CHANGED_CELL(nline, n);
