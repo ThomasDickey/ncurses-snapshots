3b7694580985cfa1514aaebbda581f6d53f465cc
Thomas E. Dickey <dickey@invisible-island.net>
2004-10-03 00:47:31 -0400

    snapshot of project "ncurses", label v5_4_20041002

--- dist.mk
+++ dist.mk
@@ -1,4 +1,4 @@
-# $Id: dist.mk,v 1.436 2004/09/25 15:58:24 tom Exp $
+# $Id: dist.mk,v 1.437 2004/10/02 19:52:47 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -10,7 +10,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 4
-NCURSES_PATCH = 20040925
+NCURSES_PATCH = 20041002
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
