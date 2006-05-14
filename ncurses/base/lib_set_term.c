bf924e22a0823407c6bb70348338300bf2c41bbe
Thomas E. Dickey <dickey@invisible-island.net>
2006-05-13 23:47:25 -0400

    snapshot of project "ncurses", label v5_5_20060513

--- ncurses/base/lib_set_term.c
+++ ncurses/base/lib_set_term.c
@@ -44,7 +44,7 @@
 #include <term.h>		/* cur_term */
 #include <tic.h>
 
-MODULE_ID("$Id: lib_set_term.c,v 1.89 2006/01/21 23:45:52 tom Exp $")
+MODULE_ID("$Id: lib_set_term.c,v 1.90 2006/05/13 19:51:14 tom Exp $")
 
 NCURSES_EXPORT(SCREEN *)
 set_term(SCREEN *screenp)
@@ -221,6 +221,7 @@ _nc_setupscreen(int slines,
 {
     int bottom_stolen = 0;
     int i;
+    bool support_cookies = USE_XMC_SUPPORT;
 
     T((T_CALLED("_nc_setupscreen(%d, %d, %p, %d, %d)"),
        slines, scolumns, output, filtered, slk_format));
@@ -391,15 +392,30 @@ _nc_setupscreen(int slines,
     SP->_panelHook.stdscr_pseudo_panel = (struct panel *) 0;
 
     /*
-     * If we've no magic cookie support, we suppress attributes that xmc
-     * would affect, i.e., the attributes that affect the rendition of a
-     * space.
+     * If we've no magic cookie support, we suppress attributes that xmc would
+     * affect, i.e., the attributes that affect the rendition of a space.
      */
     SP->_ok_attributes = termattrs();
     if (has_colors()) {
 	SP->_ok_attributes |= A_COLOR;
     }
-    if (magic_cookie_glitch > 0) {
+#if USE_XMC_SUPPORT
+    /*
+     * If we have no magic-cookie support compiled-in, or if it is suppressed
+     * in the environment, reset the support-flag.
+     */
+    if (magic_cookie_glitch >= 0) {
+	if (getenv("NCURSES_NO_MAGIC_COOKIE") != 0) {
+	    support_cookies = FALSE;
+	}
+    }
+#endif
+
+    if (!support_cookies && magic_cookie_glitch >= 0) {
+	T(("will disable attributes to work w/o magic cookies"));
+    }
+
+    if (magic_cookie_glitch > 0) {	/* tvi, wyse */
 
 	SP->_xmc_triggers = SP->_ok_attributes & (
 						     A_STANDOUT |
@@ -414,7 +430,7 @@ _nc_setupscreen(int slines,
 #if 0
 	/*
 	 * We "should" treat colors as an attribute.  The wyse350 (and its
-	 * clones) appear to be the only ones that have colors and magic
+	 * clones) appear to be the only ones that have both colors and magic
 	 * cookies.
 	 */
 	if (has_colors()) {
@@ -428,21 +444,37 @@ _nc_setupscreen(int slines,
 	 * Supporting line-drawing may be possible.  But make the regular
 	 * video attributes work first.
 	 */
-	acs_chars = 0;
-	ena_acs = 0;
-	enter_alt_charset_mode = 0;
-	exit_alt_charset_mode = 0;
+	acs_chars = ABSENT_STRING;
+	ena_acs = ABSENT_STRING;
+	enter_alt_charset_mode = ABSENT_STRING;
+	exit_alt_charset_mode = ABSENT_STRING;
 #if USE_XMC_SUPPORT
 	/*
-	 * To keep this simple, suppress all of the optimization hooks
-	 * except for clear_screen and the cursor addressing.
+	 * To keep the cookie support simple, suppress all of the optimization
+	 * hooks except for clear_screen and the cursor addressing.
 	 */
-	clr_eol = 0;
-	clr_eos = 0;
-	set_attributes = 0;
-#else
-	magic_cookie_glitch = ABSENT_NUMERIC;
+	if (support_cookies) {
+	    clr_eol = ABSENT_STRING;
+	    clr_eos = ABSENT_STRING;
+	    set_attributes = ABSENT_STRING;
+	}
 #endif
+    } else if (magic_cookie_glitch == 0) {	/* hpterm */
+    }
+
+    /*
+     * If magic cookies are not supported, cancel the strings that set
+     * video attributes.
+     */
+    if (!support_cookies && magic_cookie_glitch >= 0) {
+	magic_cookie_glitch = ABSENT_NUMERIC;
+	set_attributes = ABSENT_STRING;
+	enter_blink_mode = ABSENT_STRING;
+	enter_bold_mode = ABSENT_STRING;
+	enter_dim_mode = ABSENT_STRING;
+	enter_reverse_mode = ABSENT_STRING;
+	enter_standout_mode = ABSENT_STRING;
+	enter_underline_mode = ABSENT_STRING;
     }
 
     /* initialize normal acs before wide, since we use mapping in the latter */
