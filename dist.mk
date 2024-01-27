cf3302e623fe136c3e8aab9bf246926f6aa17a41
Thomas E. Dickey <dickey@invisible-island.net>
2024-01-27 22:04:17 +0000

    snapshot of project "ncurses", label v6_4_20240127

--- dist.mk
+++ dist.mk
@@ -26,7 +26,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1594 2024/01/20 11:52:33 tom Exp $
+# $Id: dist.mk,v 1.1595 2024/01/27 11:04:16 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -38,7 +38,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 4
-NCURSES_PATCH = 20240120
+NCURSES_PATCH = 20240127
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
--- package/ncurses.spec
+++ package/ncurses.spec
@@ -1,7 +1,7 @@
 Summary: shared libraries for terminal handling
 Name: ncurses6
 Version: 6.4
-Release: 20240120
+Release: 20240127
 License: X11
 Group: Development/Libraries
 URL: https://invisible-island.net/ncurses/
