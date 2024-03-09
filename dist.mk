7c7da8426ba4c513bf6f7f5eac2abe1c3b586dbc
Thomas E. Dickey <dickey@invisible-island.net>
2024-03-09 22:33:41 +0000

    snapshot of project "ncurses", label v6_4_20240309

--- dist.mk
+++ dist.mk
@@ -26,7 +26,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1600 2024/03/02 11:13:41 tom Exp $
+# $Id: dist.mk,v 1.1601 2024/03/09 13:05:08 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -38,7 +38,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 4
-NCURSES_PATCH = 20240302
+NCURSES_PATCH = 20240309
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
--- package/mingw-ncurses.nsi
+++ package/mingw-ncurses.nsi
@@ -1,4 +1,4 @@
-; $Id: mingw-ncurses.nsi,v 1.635 2024/03/02 11:13:41 tom Exp $
+; $Id: mingw-ncurses.nsi,v 1.636 2024/03/09 13:05:08 tom Exp $
 
 ; TODO add examples
 ; TODO bump ABI to 6
@@ -10,7 +10,7 @@
 !define VERSION_MAJOR "6"
 !define VERSION_MINOR "4"
 !define VERSION_YYYY  "2024"
-!define VERSION_MMDD  "0302"
+!define VERSION_MMDD  "0309"
 !define VERSION_PATCH ${VERSION_YYYY}${VERSION_MMDD}
 
 !define MY_ABI   "5"
